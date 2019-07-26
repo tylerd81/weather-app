df7ee45a578fe9b2602fadb218dab1a4a771cdf1
Tyler D <tyler@td81.me>
2019-07-26 16:24:42 -0400

    cleaner api and split project up

--- src/api.js
+++ src/api.js
@@ -1,35 +1,80 @@
 const apiKey = "a810861bf076b52f064ad589d2a225d8";
+const numForecast = 40;
 
-export const getWeatherData = async zip => {
-  const url = `https://api.openweathermap.org/data/2.5/forecast?zip=${zip}&cnt=35&units=imperial&appid=${apiKey}`;
+export const getCurrentForecast = async zip => {
+  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?zip=${zip}&units=imperial&appid=a810861bf076b52f064ad589d2a225d8`;
 
-  const response = await fetch(url);
+  const response = await fetch(apiUrl);
+
+  if (response.ok) {
+    const weatherData = await response.json();
+    const forecastData = {
+      current_temp: weatherData.main.temp,
+      temp_min: weatherData.main.temp_min,
+      temp_max: weatherData.main.temp_max,
+      description: weatherData.weather[0].description,
+      icon: weatherData.weather[0].icon,
+      city: weatherData.name
+    };
+    return forecastData;
+  } else {
+    return {
+      msg: "API Error",
+      forecast: null
+    };
+  }
+};
+
+// getFiveDayForecast returns an object with an array of objects which
+// have the date for the property name. Each array contains the
+// weather data provided by the openweatherapi.
+export const getFiveDayForecast = async zip => {
+  const apiUrl = `https://api.openweathermap.org/data/2.5/forecast?zip=${zip}&cnt=${numForecast}&units=imperial&appid=${apiKey}`;
+
+  const response = await fetch(apiUrl);
+  const forecastData = {};
 
   if (response.ok) {
     const data = await response.json();
-    const { list } = data;
+    const weatherData = data.list;
 
-    //loop through the list of data and get at least 4 different dates
-    let prevDate;
-    const forecast = list.reduce((items, item) => {
-      let date;
+    forecastData.city = data.city.name;
+    forecastData.msg = "Ok";
 
-      if (typeof prevDate === "undefined") {
-        //first data
-        prevDate = item.dt_txt.substring(8, 10);
-        items.push(item);
-      } else {
-        date = item.dt_txt.substring(8, 10);
-        if (date !== prevDate) {
-          prevDate = date;
-          items.push(item);
-        }
+    // loop through the list and create an object with properties set
+    // to the dates
+
+    // openweather returns an array of data, one item for every 3 hours, in the list
+    // property of the json object that is returned.
+
+    weatherData.forEach(data => {
+      // get the date
+      // the string returned is in the format YYYY-MM-DD HH:MM:SS"
+
+      let date = data.dt_txt.substring(0, 10);
+      let time = data.dt_txt.substring(11, 16);
+
+      if (typeof forecastData[date] === "undefined") {
+        forecastData[date] = []; // create a new array for the new date
       }
-      return items;
-    }, []);
 
-    return forecast;
+      let currentForecast = {
+        temp: data.main.temp,
+        temp_min: data.main.temp_min,
+        temp_max: data.main.temp_max,
+        humidity: data.main.humidity,
+        description: data.weather[0].description,
+        icon: data.weather[0].icon,
+        time
+      };
+      forecastData[date].push(currentForecast);
+    });
   } else {
-    return { msg: "Server Error" };
+    return {
+      msg: "ServerError",
+      forecast: null
+    };
   }
+
+  return forecastData;
 };
--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
 import "./css/style.css";
+
+// import Run from "./weather";
+// Run();
+import { getFiveDayForecast, getCurrentForecast } from "./api";
+
+getFiveDayForecast(12303).then(forecast => console.log(forecast));
+getCurrentForecast(12303).then(forecast => console.log(forecast));
